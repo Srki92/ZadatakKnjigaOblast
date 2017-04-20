30dfa1b5d57b4bdd0cf8fdead6f7723dc7911ba5
Srki92 <radosavljevicsrdjan92@gmail.com>
2017-04-20 19:12:53 +0200

    Kreirane klase za zadatke closes #3

---
+++ src/main/java/zadaci/Zadatak2DodavanjeVrednosti.java
@@ -0,0 +1,37 @@
+package zadaci;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ * Created by androiddevelopment on 20.4.17..
+ */
+public class Zadatak2DodavanjeVrednosti {
+
+    public static void main(String[] args) {
+
+
+
+        Connection c = null;
+        Statement stmt = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:knjigaOblast.db");
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            try{
+                c.close();
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        System.out.println("Uspesno konektovano na bazu");
+
+    }
+}
